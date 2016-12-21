c57adbfc287863d912bf2ae137c6fa3c608ecf79
Mike Hurt <mike.hurt@primarysite.net>
2016-12-21 16:28:44 +0000

    frontadmin templates

--- postoffice/frontadmin/urls.py
+++ postoffice/frontadmin/urls.py
@@ -5,8 +5,14 @@ from django.conf.urls import url
 # Local
 from .views import ArticleFrontadmin
 from .views import FrontadminIndex
+from .views import SectionFrontadmin
+from .views import SectionMemberFrontadmin
+from .views import TopicFrontadmin
 
 urlpatterns = [
     url(r'^$', FrontadminIndex.as_view(), name='frontadmin.index'),
     url(r'articles/', include(ArticleFrontadmin().get_url_patterns())),
+    url(r'sections/', include(SectionFrontadmin().get_url_patterns())),
+    url(r'sectionmembers/', include(SectionMemberFrontadmin().get_url_patterns())),
+    url(r'topics/', include(TopicFrontadmin().get_url_patterns())),
     ]
--- postoffice/frontadmin/views.py
+++ postoffice/frontadmin/views.py
@@ -1,9 +1,12 @@
 # 3rd-party
-from vanilla import TemplateView
+from jam.views import CRUDViewSet
 
 # Project
 from articles.models import Article
-from jam.views import CRUDViewSet
+from sections.models import Section
+from sections.models import SectionMember
+from topics.models import Topic
+from vanilla import TemplateView
 
 
 class FrontadminIndex(TemplateView):
@@ -14,3 +17,18 @@ class FrontadminIndex(TemplateView):
 class ArticleFrontadmin(CRUDViewSet):
     model = Article
     fields = ('title', 'content', 'status', 'topic')
+
+
+class SectionFrontadmin(CRUDViewSet):
+    model = Section
+    fields = ('name', 'slug', 'description', )
+
+
+class SectionMemberFrontadmin(CRUDViewSet):
+    model = SectionMember
+    fields = ('section', 'member', )
+
+
+class TopicFrontadmin(CRUDViewSet):
+    model = Topic
+    fields = ('name', 'slug', 'description', )
